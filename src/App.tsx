bc5c320f3c215b3cb6b8e23c8c86995395eb5f5d
AgeevDmitryMinsk <ageev.dmitry@outlook.com>
2022-03-04 14:51:19 +0300

    first initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {v1} from "uuid";
+
+type tasksType = {
+  id: string
+  title: string
+  isDone: boolean
+}
+
+const tasks: tasksType[]= [
+  {id: v1(), title:"1ое дело", isDone: true},
+  {id: v1(), title:"2ое дело", isDone: false},
+  {id: v1(), title:"3ее дело", isDone: false},
+  {id: v1(), title:"4е дело", isDone: false},
+  {id: v1(), title:"5е дело", isDone: false},
+  {id: v1(), title:"6е дело", isDone: false},
+  {id: v1(), title:"7е дело", isDone: false},
+]
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      test
     </div>
   );
 }
