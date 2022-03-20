1cee51aa980fe2e1cd60760a524475a0d968882d
AgeevDmitryMinsk <ageev.dmitry@outlook.com>
2022-03-20 19:15:39 +0300

    add . at 226

--- src/App.tsx
+++ src/App.tsx
@@ -223,7 +223,7 @@ function App() {
 		<div className="App">
 			<ButtonAppBar/>
 
-			<h2> Todo Week Schedule + 11 Unit Tests(React/TypeScript)</h2>
+			<h2> Todo Week Schedule + 11 Unit Tests(React/TypeScript).</h2>
 			<div>
 				<AddItemForm addItem={addNewTodoList}/>
 			</div>
